85623049df7df236dffcd81e1fde4fbf9d88f7d7
ethan <ethanmurray180@gmail.com>
2025-11-25 16:29:48 +0000

    added new dice roll

--- Dice.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Dice:
-    def __init__(self, sides=6):
-        self.sides = sides
-        self.value = None
-
-    def roll(self):
-        self.value = random.randint(1, self.sides)
---
+++ Roll.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Roll:
+    def __init__(self, num_dice=5, sides=6):
+        self.num_dice = num_dice
+        self.sides = sides
+        self.dice = np.zeros(num_dice, dtype=int)
+
+    def roll_all(self):
+        self.dice = np.random.randint(1, self.sides + 1, size=self.num_dice)
+
+    def roll_selected(self, indices_mask):
+        if np.sum(indices_mask) == self.num_dice:
+            self.roll_all()
+            return
+
+        tmp = self.dice
+        self.dice = np.where(np.array(indices_mask) == 1, np.random.randint(1, self.sides + 1, size=self.num_dice), tmp)
